4ebfb0d3338cf9734d679cdedfc4bb5532ec7869
yousefmankoulah <45926659+yousefmankoulah@users.noreply.github.com>
2024-02-11 15:51:54 +0000

    c

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,5 +1,26 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Home } from "./pages/Home"
+import { About } from "./pages/About"
+import { Dashboard } from "./pages/Dashboard"
+import { Signin } from "./pages/SignIn"
+import { Signup } from "./pages/Signup"
+import { Projects } from "./pages/Projects"
+
+
 export default function App () {
   return(
-    <h1 className="text-3xl text-red-500">App</h1>
-  )
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/projects" element={<Projects />} />
+
+   
+        </Routes>
+      </BrowserRouter>
+
+    )
 }
