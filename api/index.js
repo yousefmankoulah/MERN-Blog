590a9e787193a2744f5b0dc65d864ed046021268
yousefmankoulah <45926659+yousefmankoulah@users.noreply.github.com>
2024-02-12 02:15:43 +0000

    create test api

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 dotenv.config()
+import userRoute from './routes/user.route.js'
 
 
 mongoose.connect(process.env.MONGO_URL).then(() => {
@@ -11,5 +12,8 @@ mongoose.connect(process.env.MONGO_URL).then(() => {
 })
 
 const app = express()
+//routes
+app.use('/api/user', userRoute)
+
 
 app.listen(3000, () => console.log('Listening on port 3000'))
