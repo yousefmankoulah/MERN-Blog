be83da45e9810a7bff5acb2dda8c10c2eba6cd1e
yousefmankoulah <45926659+yousefmankoulah@users.noreply.github.com>
2024-02-12 01:43:11 +0000

    connect to server

--- Client/src/components/Header.jsx
+++ Client/src/components/Header.jsx
@@ -8,8 +8,7 @@ export function Header() {
     return (
         <Navbar className='border-b-2'>
             <Link to="/" className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'>
-                <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>Mankoulah</span>
-
+                <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>Mankoulah</span>Blog
             </Link>
             <form>
                 <TextInput 
@@ -26,7 +25,7 @@ export function Header() {
             <div className='flex gap-2 md:order-2'>
                 <Button className='w-12 h-10 hidden sm:inline' color='gray' pill><FaMoon /></Button>
                 <Link to="/signin">
-                <Button gradientDuoTone='purpleToBlue'>Sign In</Button>
+                <Button gradientDuoTone='purpleToBlue' outline>Sign In</Button>
                 </Link>
                 <Navbar.Toggle />
             </div>
