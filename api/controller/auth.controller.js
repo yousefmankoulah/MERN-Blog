9bef08b7d72c03ff01fa44aa6e3274fcf92449fb
yousefmankoulah <45926659+yousefmankoulah@users.noreply.github.com>
2024-02-12 21:14:23 +0000

    sign in backend

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js"
 import bcryptjs from 'bcryptjs'
 import { errorHandler } from "../utilis/error.js"
+import jwt from 'jsonwebtoken'
 
 export const signup = async (req, res, next) => {
     const { username, email, password } = req.body
@@ -23,9 +24,44 @@ export const signup = async (req, res, next) => {
     
     try{
         await newUser.save()
+        res.json('User created successfully')
+
     } catch(err){
         next(err)
     }
 
-    res.status(201).json({ message: 'User created successfully' })
+}
+
+
+export const signin = async (req, res, next) => {
+    const { email, password } = req.body;
+
+    if(!email || !password || email === '' || password === ''){
+        next(errorHandler(400, 'All Fields are required'))
+    }
+
+    try{
+
+        const validator = await User.findOne({ email })
+        if(!validator){
+            return next(errorHandler(400, "User not Found"))
+        }
+        const validpassword = bcryptjs.compareSync(password, validator.password)
+        if (!validpassword){
+            return next(errorHandler(400, "Invalid Password"))
+        }
+
+        const token = jwt.sign({id: validator._id}, process.env.SECRET_KEY)
+        const { password: pass, ...rest } = validator._doc;
+
+        res
+        .status(200)
+        .cookie('access_token', token, {
+          httpOnly: true,
+        })
+        .json(rest);
+        } catch (err){
+        next(err)
+    }
+
 }
